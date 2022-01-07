4cbdb88361c590b96a9999190d19fbeeba96d5aa
Magma-Flavio Portela <fmspx@hotmail.com>
2022-01-06 22:00:01 -0300

    melhoria identificacao do metodo

--- src/private/p_qrpc_listen_qrpc_slot.cpp
+++ src/private/p_qrpc_listen_qrpc_slot.cpp
@@ -22,6 +22,7 @@ public:
     QRPCListenQRPC*listenQRPC=nullptr;
     QList<const QMetaObject*> listenQRPCControllers;
     QList<const QMetaObject*> listenQRPCParserRequest;
+
     explicit QRPCListenQRPCSlotPvt(QRPCListenQRPCSlot*slot, QRPCListenQRPC*listenQRPC) : QObject(slot)
     {
         QObject::connect(slot, &QRPCListenQRPCSlot::requestInvoke, this, &QRPCListenQRPCSlotPvt::onRequestInvoke);
@@ -30,6 +31,7 @@ public:
         this->listenQRPCParserRequest=listenQRPC->server()->parsers();
         this->controllerRouter=QRPCControllerRouter::newRouter(this);
     }
+
     virtual ~QRPCListenQRPCSlotPvt()
     {
     }
